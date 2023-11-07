8ab031e8597cfd4e07ff170cc5efd82fa68bcd29
Bohan Li <bohan.li@circle.com>
2023-11-07 14:34:44 -0800

    Customize user agent for sample app

--- w3s-ios-sample-app-wallets/w3s-ios-sample-app-wallets/WalletSdkAdapter.swift
+++ w3s-ios-sample-app-wallets/w3s-ios-sample-app-wallets/WalletSdkAdapter.swift
@@ -25,6 +25,7 @@ class WalletSdkAdapter {
 
         WalletSdk.shared.setLayoutProvider(self)
         WalletSdk.shared.setDelegate(self)
+        WalletSdk.shared.customUserAgent = "PW-IOS-SAMPLE-APP"
     }
 
     func updateEndPoint(_ endPoint: String, appId: String) {
